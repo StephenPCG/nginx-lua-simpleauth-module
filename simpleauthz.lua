8030acec9ac768ea1e9fc8e6c9b76a5ab0df2128
Zhang Cheng <StephenPCG@gmail.com>
2014-01-04 10:40:59 +0800

    Initial commit, add simpleauthz

---
+++ simpleauthz.lua
@@ -0,0 +1,89 @@
+local groups = {}
+local rule_actions = {}
+local rule_roles = {}
+local rule_except_roles = {}
+
+local function expand_roles (roles)
+    -- recursively expand list of group names and uids to a list of pure uids
+    local ret = {}
+    for k, v in pairs(roles) do
+        if type(k) == "string" then
+            name = k
+        else
+            name = v
+        end
+        if string.sub(name, 1, 1) == "@" then
+            local group_name = string.sub(name, 2, string.len(name))
+            local group_roles = groups[group_name]
+            if group_roles ~= nil then
+                for _name, _ in pairs(expand_roles(group_roles)) do
+                    ret[_name] = true
+                end
+            end
+        else
+            ret[name] = true
+        end
+    end
+    return ret
+end
+
+local function create_group (group_name, ...)
+    -- create a group of uids
+
+    groups[group_name] = expand_roles({...})
+end
+
+local function create_rule (rule_name, action, roles, except_roles)
+    -- create a rule
+    -- 'action': can be 'allow' or 'deny'
+    -- 'roles': list of uids and group_names
+    -- uid or group listed in 'roles' will be 'action'ed, unless the uid is listed in 'except_roles'.
+
+    rule_roles[rule_name] = expand_roles(roles)
+    rule_except_roles[rule_name] = expand_roles(except_roles)
+    rule_actions[rule_name] = (action == "allow")
+end
+
+local function access (rule_name, uid)
+    -- do authz process, be invoked by access_by_lua:
+    --   access_by_lua 'simpleauthz.auth(rule_name, uid)';
+
+    if uid == nil then
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+    end
+
+    action = rule_actions[rule_name]
+    roles = rule_roles[rule_name]
+    except_roles = rule_except_roles[rule_name]
+
+    -- by default , if the rule_name does not exist, forbid access
+    if action == nil or roles == nil or except_roles == '' then
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+    end
+
+    if action then
+        -- rule action == "allow"
+        if roles[uid] and not except_roles[uid] then
+            ngx.exit(ngx.HTTP_OK)
+        end
+        ngx.exit(ngx.HTTP_FORBIDDEN)
+    else
+        -- rule action == "deny"
+        if roles[uid] and not except_roles[uid] then
+            ngx.exit(ngx.HTTP_FORBIDDEN)
+        end
+        ngx.exit(ngx.HTTP_OK)
+    end
+end
+
+-- special rules to allow/deny all (logged in) users
+create_rule ('ALLOW_ALL', 'deny', {}, {})
+create_rule ('DENY_ALL', 'allow', {}, {})
+
+local P = {
+    create_group = create_group,
+    create_rule = create_rule,
+    access = access
+}
+
+return P
